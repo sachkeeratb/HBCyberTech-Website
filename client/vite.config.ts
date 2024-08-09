e06207873f68696078c474ca7b91e4f632c6fcc1
Sachkeerat Brar <sachkeeratbrar@gmail.com>
2024-08-08 23:38:54 -0400

    Fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-// https://vitejs.dev/config/
->>>>>>> 8fbf333 (First commit)
-=======
->>>>>>> 3dde0c9 (Add a server folder)
 export default defineConfig({
 	plugins: [react()],
 	server: {
