8f3b6f3a552afd4b5d1e43f0aa3528ee43f80a80
Sachkeerat Brar <sachkeeratbrar@gmail.com>
2024-08-08 23:24:06 -0400

    First commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+<<<<<<< HEAD
+=======
+// https://vitejs.dev/config/
+>>>>>>> 8fbf333 (First commit)
 export default defineConfig({
 	plugins: [react()],
 	server: {
