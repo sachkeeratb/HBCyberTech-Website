68fb32ad7dc66cb357284685c88f91245757f5a7
Sachkeerat Brar <sachkeeratbrar@gmail.com>
2024-08-08 23:32:43 -0400

    Add a server folder

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 // https://vitejs.dev/config/
 >>>>>>> 8fbf333 (First commit)
+=======
+>>>>>>> 3dde0c9 (Add a server folder)
 export default defineConfig({
 	plugins: [react()],
 	server: {
